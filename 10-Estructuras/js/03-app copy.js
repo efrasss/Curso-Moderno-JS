d1edb7fa24c0b3852c6e654cbd85162ffbf0f014
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2024-06-27 16:26:11 -0600

    Mayor o igual y else if

---
+++ 10-Estructuras/js/03-app copy.js
@@ -0,0 +1,11 @@
+// Operador Mayor que y menor que
+
+const dinero = 100;
+const totalAPagar = 500;
+
+if ( dinero >= totalAPagar ) {
+    console.log('Si podemos pagar');
+} else {
+    console.log('Fondos insuficientes');
+};
+
