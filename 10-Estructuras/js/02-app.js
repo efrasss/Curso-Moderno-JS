5dfd133a3c76b2ded37050a00a4080344171fb7a
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2024-06-27 13:06:39 -0600

    Comparador estricto

---
+++ 10-Estructuras/js/02-app.js
@@ -0,0 +1,18 @@
+const puntaje = 1000;
+const puntaje2 = "1000";
+
+console.log(typeof puntaje);
+console.log(typeof puntaje2);
+
+// if (puntaje != 1000) { // != diferente == igual a
+//     console.log('Si es diferente...')
+// }
+
+if (puntaje === 1000) { // != diferente == igual a
+    console.log('Si es igual!');
+} else {
+    console.log('no es igual');
+}
+
+// == Operador que no es estricto
+// === Operador estricto
