9a6df9009eef7745df409ce2253fdfdb85090cde
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2024-06-27 09:21:00 -0600

    Arrow Functions

---
+++ 09-Funciones/js/10-app.js
@@ -0,0 +1,9 @@
+// Arrow Functions
+
+const aprendiendo = () => {
+    console.log(`Aprendiendo JavaScript`);
+}
+
+const aprendiendo2 = () => 'Aprendiendo JavaScript';
+
+console.log(aprendiendo2());
